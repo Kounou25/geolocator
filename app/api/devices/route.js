b6e672d888b8b433cd3ff0a37c73554855f1ba9e
kounou25 <kounougilbert288@gmail.com>
2024-12-18 22:08:28 +0100

    requeperation de la derniere position de chaque appareil: okay

--- app/api/devices/route.js
+++ app/api/devices/route.js
@@ -3,7 +3,7 @@ import { query } from '../../lib/db';
 // Récupérer tous les items
 export async function GET(request) {
   try {
-    const result = await query("select * from plugin_devicelog_log where severity='VERBOSE' and message LIKE '%GPS%' ", []);
+    const result = await query("SELECT DISTINCT ON (dev.number) log.id, dev.number, log.deviceid, log.ipaddress, log.severity, log.message, to_timestamp(log.createtime / 1000) AS createtime FROM plugin_devicelog_log log JOIN devices dev ON log.deviceid = dev.id WHERE log.severity = 'VERBOSE' AND log.message LIKE '%GPS%' ORDER BY dev.number, log.createtime DESC ", []);
     return new Response(JSON.stringify(result.rows), { status: 200 });
   } catch (err) {
     return new Response(JSON.stringify({ error: 'Erreur lors de la récupération' }), { status: 500 });
