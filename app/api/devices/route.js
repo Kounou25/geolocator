491da9817ce48aaad01a66a42a03c7513500d728
kounou25 <kounougilbert288@gmail.com>
2024-12-18 20:09:50 +0100

    request error

--- app/api/devices/route.js
+++ app/api/devices/route.js
@@ -3,7 +3,7 @@ import { query } from '../../lib/db';
 // Récupérer tous les items
 export async function GET(request) {
   try {
-    const result = await query("select * from plugin_devicelog_log where severity='VERBOSE' and message LIKE '%GPS%' ", []);
+    const result = await query("select * from plugin_devicelog_log ", []);
     return new Response(JSON.stringify(result.rows), { status: 200 });
   } catch (err) {
     return new Response(JSON.stringify({ error: 'Erreur lors de la récupération' }), { status: 500 });
