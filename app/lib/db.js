197dba3b26e45c3584db600dc5a8d5df171d9766
kounou25 <kounougilbert288@gmail.com>
2024-12-18 20:48:38 +0100

    connexion a la base de donnee:ok

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'goliat', // Votre utilisateur PostgreSQL
-  host: 'localhost', // L'hôte de votre serveur PostgreSQL
+  host: 'predator.urbanagence.com', // L'hôte de votre serveur PostgreSQL
   database: 'hmdm', // Le nom de votre base de données
   password: 'goliat@predator', // Laissez vide si votre base de données n'a pas de mot de passe
   port: 5432, // Port par défaut de PostgreSQL
