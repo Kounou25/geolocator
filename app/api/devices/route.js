e715b114ebd9fae68dcf9b748846fb40a1542fa5
kounou25 <kounougilbert288@gmail.com>
2024-12-18 20:48:10 +0100

    mise en place de l'api: okay

--- app/api/devices/route.js
+++ app/api/devices/route.js
@@ -3,7 +3,7 @@ import { query } from '../../lib/db';
 // Récupérer tous les items
 export async function GET(request) {
   try {
-    const result = await query("select * from plugin_devicelog_log ", []);
+    const result = await query("select * from plugin_devicelog_log where severity='VERBOSE' and message LIKE '%GPS%' ", []);
     return new Response(JSON.stringify(result.rows), { status: 200 });
   } catch (err) {
     return new Response(JSON.stringify({ error: 'Erreur lors de la récupération' }), { status: 500 });
